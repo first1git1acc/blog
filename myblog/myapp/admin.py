d6fd9b2751b206f33b492979d8cc39af227573e1
LENOVO <LENOVO@DESKTOP-BH2L40U>
2024-02-19 15:25:33 +0200

    create model Post and modelManager

--- myblog/myapp/admin.py
+++ myblog/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author'] #додає колонку із фільтрами для полів
+    raw_id_fields = ['author'] #замість списку із юзерів створює поле пошуку юзера
--- myblog/myapp/models.py
+++ myblog/myapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) #auto_now_add зберігає дату при створенні обєкта
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,choices=Status.choices,default = Status.DRAFT)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')#звязок багато до одного
+
+    objects = models.Manager() #менеджер за замовчуванням
+    published = PublishedManager() #менеджер для виконання операцій із опублікованими постами
+
+    def __str__(self):
+        return self.title
